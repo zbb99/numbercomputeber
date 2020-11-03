a8a1e267b4bbcb22ff2e88a2aba1f917f140f6fe
zbb <1608054854@qq.com>
2020-11-03 15:57:47 +0800

    去除无用项

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path=require("path")
-module.exports={
-    entry:__dirname+'/index.js',
-    output:  {
-        path:path.resolve(__dirname,'dist'),
-        filename: ' numbercomputeber.bundle.js'
-    }
-}
